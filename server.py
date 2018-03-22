94606e24b22dddbace1418f9e7f783defdccb925
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2018-03-22 14:57:55 -0700

    Enviando comandos al cliente

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import sockets
+import socket
 import sys
 
 # creamos el socket (permite conectar 2 computadoras)
@@ -19,7 +19,7 @@ def socket_bind():
         global host
         global port
         global s #socket
-        print("Binding socket to port" + str(port))
+        print("Binding socket to port " + str(port))
         s.bind((host, port))
         s.listen(5)
     except socket.error as msg:
@@ -32,3 +32,25 @@ def socket_accept():
     print("Connection has been establish |" +  "IP " + address[0] + " | Port" + str(address[1]))
     send_commands(conn)
     conn.close()
+
+# Enviando comandos
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), "utf-8")
+            print(client_response, end="")
+
+def main():
+    socket_create()
+    socket_bind()
+    socket_accept()
+
+
+main()
+
